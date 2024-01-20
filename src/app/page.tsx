97ead013a5c58de5a4c6a666986699bd42ef24a2
Sprechblase <hi@skre.dev>
2024-01-20 19:24:43 +0100

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import { ModeToggle } from '@/components/ui/toggle-mode'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main>
+      <header className='sm:flex sm:justify-between py-1 px-4 border-b'>
+        <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full'>
+          <div className='flex items-center'>
+            <Link href="/" className='ml-4 lg:ml-0'>
+              <h1 className='text-xl font-bold'>
+                skre/dashboard
+              </h1>
+            </Link>
+          </div>
+          <ModeToggle />
+        </div>
+      </header>
     </main>
   )
 }
