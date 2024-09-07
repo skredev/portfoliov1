0df3a5f657a7fbda091a645810df9d066ff3144a
Samuel Krebs <hi@skre.dev>
2024-09-07 16:12:59 +0200

    fix: hidden removed from settings
    Hidden caused the disappearance from the settings on phones

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -39,7 +39,7 @@ export default function SettingsLayout({
 
   if (isUserValid) {
     return(
-      <div className="hidden space-y-6 pt-12 p-10 pb-16 md:block px-48 max-[1280px]:px-24 max-[768px]:px-12">
+    <div className="space-y-6 pt-12 p-10 pb-16 md:block px-48 max-[1280px]:px-24 max-[768px]:px-12">
       <div className="space-y-0.5">
         <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
         <p className="text-muted-foreground">
